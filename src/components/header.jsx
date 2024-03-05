828f721efd02e29cf427b4023166aed4b522a4c2
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-03-05 00:18:15 +0000

    Adding logo and button to navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './header.css';
+import logo from '../assets/portfolio-logo.png';
 
 const Navbar = () => {
 
-
     return (
-        <div>navbar</div>
+        <nav className="navbar">
+
+            <div className='desktopMenu'>
+            <img src='{logo}' alt='Logo' className='logo'/>
+            </div>
+                <button className="desktopMenuBtn">
+                    <img src="" alt="" className="desktopMenuIcon" />Reach Out!</button>
+        </nav>
     )
 }
 
